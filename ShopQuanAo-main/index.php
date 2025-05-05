fd64128653d0cc13f4191445a63c532cfb90ff76
trungboro116 <trungboro116@gmail.com>
2025-05-06 00:41:18 +0700

    ok

--- ShopQuanAo-main/index.php
+++ ShopQuanAo-main/index.php
@@ -139,17 +139,18 @@
     <section class="hero">
         <div class="hero__slider owl-carousel">
             <div class="hero__items set-bg" data-setbg="img/hero/hero-1.jpg">
+                <picture>
+                    <source media="(max-width: 576px)" srcset="img/hero/hero-1-mobile.jpg">
+                    <source media="(max-width: 768px)" srcset="img/hero/hero-1-tablet.jpg">
+                    <img src="img/hero/hero-1.jpg" alt="Hero Image 1" style="display: none;">
+                </picture>
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-5 col-lg-7 col-md-8">
                             <div class="hero__text">
                                 <h6>Summer Collection</h6>
                                 <h2>Fall - Winter Collections 2030</h2>
-                                <p>
-                                    A specialist label creating luxury essentials. Ethically
-                                    crafted with an unwavering commitment to exceptional
-                                    quality.
-                                </p>
+                                
 
                                 <a href="shop.php" class="primary-btn">Shop now <span class="arrow_right"></span></a>
                                 <div class="hero__social">
@@ -164,17 +165,18 @@
                 </div>
             </div>
             <div class="hero__items set-bg" data-setbg="img/hero/hero-2.jpg">
+                <picture>
+                    <source media="(max-width: 576px)" srcset="img/hero/hero-2-mobile.jpg">
+                    <source media="(max-width: 768px)" srcset="img/hero/hero-2-tablet.jpg">
+                    <img src="img/hero/hero-2.jpg" alt="Hero Image 2" style="display: none;">
+                </picture>
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-5 col-lg-7 col-md-8">
                             <div class="hero__text">
                                 <h6>Summer Collection</h6>
                                 <h2>Fall - Winter Collections 2030</h2>
-                                <p>
-                                    A specialist label creating luxury essentials. Ethically
-                                    crafted with an unwavering commitment to exceptional
-                                    quality.
-                                </p>
+                              
 
                                 <a href="shop.php" class="primary-btn">Shop now <span class="arrow_right"></span></a>
                                 <div class="hero__social">
